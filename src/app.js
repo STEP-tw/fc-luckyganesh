fd6a218e4ed8e7c829760ceb96cd53633fc12c74
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-01-16 12:46:58 +0530

    created index.html and showed with styles for only img and gifs.
    need to implemented html page and refactor the app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
+
+const readFiles = function(req,res,fileName){
+  fs.readFile(fileName,(err,data) => {
+    if(err){
+      res.statusCode = 400;
+      console.log(req.url);
+      res.end();
+      return ;
+    }
+    res.statusCode = 200;
+    res.write(data);
+    res.end();
+  });
+}
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+  readFiles(req,res,req.url.slice(1) || "index.html");
 };
 
 // Export a function that can act as a handler
