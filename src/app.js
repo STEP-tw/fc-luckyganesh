fe89ca28380f9c46458e5db04f733350ec03f32c
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-01-16 15:53:23 +0530

    Extracted functions in app function.

--- src/app.js
+++ src/app.js
@@ -1,25 +1,49 @@
 const fs = require('fs');
 
-const readFiles = function(res,fileName){
-  fs.readFile(fileName,(err,data) => {
-    if(err){
-      res.statusCode = 400;
-      res.end();
-      return ;
-    }
-    res.statusCode = 200;
-    res.write(data);
-    res.end();
-  });
+const SERVER_ERROR = '500: Internal server Error';
+const FILE_ERROR = '400 : file not found';
+
+const send = function (res, statusCode, content) {
+  res.statusCode = statusCode;
+  res.write(content);
+  res.end();
+  return;
+}
+
+const giveResponse = function(res,err,content){
+  let statusCode = 500;
+  let data = SERVER_ERROR;
+  if (!err) {
+    statusCode = 200;
+    data = content;
+  }
+  if (err != null) {
+    statusCode = 400;
+    data = FILE_ERROR;
+  }
+  send(res,statusCode,data);
+  return;
+}
+
+const readFileContent = function (res, fileName) {
+  fs.readFile(fileName, giveResponse.bind(null,res));
+  return;
+}
+
+const defaultPaths = {
+  '/' : "/index.html"
+}
+
+const resolveFileName = function (url) {
+  let path = url;
+  path = defaultPaths[url] || url;
+  path = "./html_page" + path;
+  return path;
 }
 
 const app = (req, res) => {
-  let path = req.url;
-  if(path === "/"){
-    path = path+"index.html";
-  }
-  path = "./html_page" + path
-  readFiles(res,path);
+  const path = resolveFileName(req.url);
+  readFileContent(res, path);
 };
 
 // Export a function that can act as a handler
