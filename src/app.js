e8ea2cdcde37b37d99f9ada300d75705d421c4af
Sai Ganesh <44020019+luckyganesh@users.noreply.github.com>
2019-01-17 22:40:49 +0530

    Added guest book feature.

--- src/app.js
+++ src/app.js
@@ -27,34 +27,91 @@ const serveFileContent = function (res, fileName) {
   return;
 }
 
-const logRequest = function(req,res,next){
-  console.log(req.method,req.url);
+const logRequest = function (req, res, next) {
+  console.log(req.method, req.url);
   next();
 }
 
-const sendNotFound = function(req,res){
-  send(res,404,FILE_ERROR);
+const sendNotFound = function (req, res) {
+  send(res, 404, FILE_ERROR);
 }
 
-const resolveFileName = function(path){
-  if(path == "/"){
+const resolveFileName = function (path) {
+  if (path == "/") {
     path = '/index.html';
   }
   return `./public${path}`
 }
 
-const serveFile = function(req,res){
+const serveFile = function (req, res) {
   const path = resolveFileName(req.url);
-  serveFileContent(res,path);
+  serveFileContent(res, path);
+}
+
+const readArgs = content => {
+  let args = {};
+  const splitKeyValue = pair => pair.split("=");
+  const assignKeyValueToArgs = ([key, value]) => (args[key] = value);
+  content
+    .split("&")
+    .map(splitKeyValue)
+    .forEach(assignKeyValueToArgs);
+  return args;
+};
+
+const readBody = function (req, res, next) {
+  let data = "";
+  req.on("data", chunk => data += chunk);
+  req.on("end", () => {
+    req.body = readArgs(data)
+    next();
+  });
+}
+
+const addHtmlFormat = function (content) {
+  content = `<tr><td>${content.date}</td><td>${content.name}</td><td>${content.comment}</td></tr>`
+  return content;
+}
+
+const readComments = function (commentData) {
+  let html = ""
+  commentData = '[' + commentData.slice(0, -1) + ']';
+  commentData = JSON.parse(commentData).filter(x => x !== "");
+  commentData.forEach(x => { html = addHtmlFormat(x) + html });
+  return html;
+}
+
+const serveGuestBook = function (req, res) {
+  fs.readFile('./public/guest_book.html', (err, data) => {
+    let html = data;
+    fs.readFile('./src/commenters_data.txt', (err, data) => {
+      let content = `<tbody>${readComments(data)}</tbody>`;
+      html += content;
+      send(res, 200, html);
+    });
+  })
+}
+
+const uploadData = function (req, res) {
+  let contentToAdd = req.body;
+  contentToAdd.date =new Date();
+  contentToAdd = JSON.stringify(contentToAdd) + ","; 
+  fs.appendFile('./src/commenters_data.txt',contentToAdd,(err)=>{
+    if(err) {console.log(err); return;};
+    serveGuestBook(req,res);
+  })
 }
 
 app.use(logRequest);
-app.get('/',serveFile);
-app.get('/favicon.ico',serveFile);
-app.get('/images/freshorigins.jpg',serveFile);
-app.get('/images/animated-flower-image-0021.gif',serveFile);
-app.get('/main.js',serveFile);
-app.get('/style.css',serveFile);
+app.use(readBody);
+app.get('/', serveFile);
+app.get('/favicon.ico', serveFile);
+app.get('/images/freshorigins.jpg', serveFile);
+app.get('/images/animated-flower-image-0021.gif', serveFile);
+app.get('/main.js', serveFile);
+app.get('/style.css', serveFile);
+app.get('/guest_book.html', serveGuestBook);
+app.post('/upload_data', uploadData);
 app.use(sendNotFound);
 // Export a function that can act as a handler
 
